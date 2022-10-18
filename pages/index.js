041943d72beeb348eca8f67713ceb98b212094a1
Habibur Rahman <hrsshainik@gmail.com>
2022-10-18 13:06:33 +0600

    add mixins for media queries and add selection

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <meta name="description" content="Demo ecommerce web app." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Hello world</h1>
     </>
   );
 }
