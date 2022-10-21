67ca5706cd28b3c7523af7391353016abc069f00
Habibur Rahman <hrsshainik@gmail.com>
2022-10-22 01:36:13 +0600

    add all meta tags in every page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -8,7 +8,18 @@ const Contact = () => {
     <>
       <Head>
         <title>Contact us</title>
-        <meta name="description" content="contact page" />
+        <meta name="description" content="Demo ecommerce web app." />
+        <meta name="keywords" content="ecommerce" />
+        <meta name="author" content="Habibur Rahman" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="ecommerce" />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="" />
+        <meta name="twitter:image:alt" content="Alt text for image" />
       </Head>
 
       <Layout>
